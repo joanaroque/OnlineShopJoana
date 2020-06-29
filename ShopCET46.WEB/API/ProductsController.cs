aff7828592ef03d2e3c4bd7f176540750d98439e
joanaroque <joanaramosque@gmail.com>
2020-06-29 14:00:10 +0100

    api product

---
+++ ShopCET46.WEB/API/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopCET46.WEB.Data;
+
+namespace ShopCET46.WEB.API
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok(_productRepository.GetAll());
+        }
+    }
+}
